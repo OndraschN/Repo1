6efbe26a48413697118667c8ed87428c00fcac6e
OndraschN <aniepraschk@gmx.de>
2022-05-19 18:26:25 +0200

    Zeile 4 geändert
    Ein längerer Commit mit viel mehr Text. Vermutlich über mehr als eine Zeile.

--- FileToChange.js
+++ FileToChange.js
@@ -3,3 +3,5 @@ In dieser Datei werde ich nach dem Add noch etwas ändern.
 Add ist erfolgt. Ich ergänze diese Zeile.
 
 Danach zeigt Git sie wieder als modified an und ich muss sie erneut adden!
+
+Erneute Änderung.
